f296ac86493e33cfbdf51629557f126da14cdf18
Caroline Berg <carolineeberg@gmail.com>
2019-01-31 23:31:34 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,8 @@
+require_relative "../config/environment.rb"
+require 'active_support/inflector'
+require 'interactive_record.rb'
+
+class Student < InteractiveRecord
+  attr_accessor :id, :name
+
+end
